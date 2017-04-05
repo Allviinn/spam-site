8dec0a54e15aa758012777baaab1b1dca91dd7c3
marya <ibtissem.m@codeur.online>
2017-04-05 14:21:17 +0200

    style ibtissem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
   src="https://code.jquery.com/jquery-3.2.1.min.js"
   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
   crossorigin="anonymous"></script>
+    
+    <link href="{{ URL::to('css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection" />
 
     </head>
     <body>
@@ -13,15 +15,23 @@
         <h1 id="h1">Rapporter un numéro</h1>
         <form>
 
-            <label>Numéro : </label><br><input type="text" name="numero" id="numero"><br><br><br>
-            <label>Type : </label><br><select id="selectType"><option value="appel">Appel</option><option value="sms">SMS</option></select><br><br><br>
-            <label>Qualtié : </label><br><select id="selectQualite"><option value="positif">Positif</option><option value="negatif">Négatif</option></select><br><br><br>
-            <label>Commentaire : </label><br><textarea cols="50" rows="6" name="commentaire" id="commentaire"></textarea><br><br><br>
-            <label>Votre e-mail : </label><br><input type="text" name="email" id="email"><br><br><br>
+            <label>Numéro : </label><input type="text" name="numero" id="numero"><br><br><br>
+            
+            
+             <label>Type: </label>
+             <input type="radio" name="type" value="Sms" class="message_pri"> Sms<br>
+            <label>&nbsp;</label><input type="radio" name="type" value="Appel" class="message_pri"> Appel<br><br>
+            
+           
+            <label>Commentaire : </label><textarea cols="50" rows="6" name="commentaire" id="commentaire"></textarea><br><br><br>
+            <label>Qualité:</label><input type="radio" name="type" value="Sms" class="message_pri"> Positif<br>
+            <label>&nbsp;</label><input type="radio" name="type" value="Appel" class="message_pri"> Négatif<br><br>
+            
+            <label>Votre e-mail : </label><input type="text" name="email" id="email"><br><br><br>
             <label>Sous quelle pseudonyme voulez-vous que les autre vous voient :  </label><br><input type="text" name="pseudo" id="pseudo">
 
             <input type="hidden" id="token" name="_token" value="{{ csrf_token() }}"><br><br><br>
-            <input type="submit" id="submit" value="Inscription !">
+            <label>&nbsp;</label><input type="submit" id="submit" value="Ajouter !">
 
         </form>
 
