c1494695786d3321e91428d7d17079440857bf88
Mr. Le Bosniaque <alvin.m@codeur.online>
2017-04-06 14:33:36 +0200

    Le fonrt est bien parti

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,15 +14,14 @@
         <header>
             <div class="container-fluid">
                 <div class="row">
-                <div class="col-xs-2"><img src="graphisme/graphismes/images/icone-logo.png" width="50"></div>
+                <div class="col-xs-2"><img src="graphisme/graphismes/images/icone-logo-rouge.png" width="50"></div>
                 <div class="col-xs-offset-8 col-xs-2"><img src="graphisme/graphismes/images/menuburger.png" width="40"></div>
                 </div>
             </div>
         </header>
 
         <section id="sectionAccueil">
-            <img src="graphisme/graphismes/images/fond-accueil-mobile.png">
-            <a href="#" id="lienMobileApropos">Qui sommes-nous</a>
+            <img src="graphisme/graphismes/images/fond-accueil-mobile2.png">
 
         </section>
 
@@ -30,17 +29,31 @@
             <div class="row">
             <h4 class="col-xs-12">Ajoutez un nouveau numéro ou recherchez un numéro dans notre liste</h4>
 
-            <article id="articleAjout" class="col-xs-offset-1 col-xs-10">
+            <a href="ajoutNumero" id="lienAjoutNumero"><article id="articleAjout" class="col-xs-offset-1 col-xs-10">
                 
-                <img src="graphisme/graphismes/images/icone-ajout.png" width="50">
+                <img src="graphisme/graphismes/images/icone-ajout.png" width="70">
                 <p>Vous souhaitez déclarer un SMS ou un appel comme indésirable?</p>
-                <a href="#" id="lienAjoutNumero">Cliquez ici</a>
+                
 
-            </article>
+            </article></a>
+            </div>
+            <div class="row">
+            <a href="" id="lienRechercheNum"><article id="articleRecherche" class="col-xs-offset-1 col-xs-10">
+
+                <img src="graphisme/graphismes/images/icone-loupe.png" width="70">
+                <p>Vous souhaitez rechercher un numéro?</p>
+
+            </article></a>
             </div>
         </section>
 
-            <a href="ajoutNumero">Page dIbtissem</a>
-        <script type="text/javascript" src="{{ URL::to('js/welcome.js') }}"></script>
+         <form id="formRechercheNum" method="post" action="recherche">
+
+            <label>Entrez le numéro : </label><input type="text" name="numeroRecherche">
+            <input type="submit" name="submitRechercheNum" id="submitRechercheNum" value="Rechercher">
+            <input type="hidden" id="token1" name="_token" value="{{ csrf_token() }}">
+         </form>
+
+        <script type="text/javascript" src="{{ URL::to('js/alvin.js') }}"></script>
     </body>
 </html>
