7a564ff2a7ce53f15b8dc2a3ed189cb94086af12
Mr. Le Bosniaque <alvin.m@codeur.online>
2017-04-10 16:45:26 +0200

    page a propos commencée

--- resources/views/a_propos.blade.php
+++ resources/views/a_propos.blade.php
@@ -1,14 +1,91 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>FORMULAIRE DE SIGNALEMENT</title>
+	<title></title>
 	<meta charset="utf-8">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="{{ URL::to('css/a_propos.css') }}">
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
+		<header>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"><a href="/"><img src="graphisme/graphismes/images/icone-logo.png" width="50"></a></div>
+                    <div id="headerDiv2" class="offset-5 col-5 offset-sm-7 col-sm-4 offset-md-8 col-md-3 offset-lg-8 col-lg-2 offset-xl-7 col-xl-3"><a href="a_propos">UN SPAM ?</a></div>
+                        
+                </div>
+            </div>
+        </header>
 
+        <section id="section1" class="container-fluid">
+        <div class="row">
+        	<article id="citation" class="col-12">
+        		<p class="offset-1 col-10">"L'homme et sa sécurité doivent constituer la première préoccupation de toute aventure technologique."-Albert Einstein</p>
+        	</article>
+        </div>
+
+        
+        	<article id="aPropos">
+        	<div class="row">
+        		<div id="logo" class="col-12 offset-sm-1 col-sm-10 offset-md-2 col-md-8 offset-lg-1 col-lg-5"><img src="graphisme/graphismes/images/logo-rouge.png"></div>
+
+        		<div class="col-12 offset-sm-1 col-sm-10 offset-md-2 col-md-8 offset-lg-0 col-lg-5" id="aProposP"><p>Spaminator s’adresse aux personnes régulièrement importunées par des appels téléphoniques ou SMS appelés SPAMS, 
+        		et souhaitant les signaler à d’autres utilisateurs. Le site vous permet de rechercher rapidement un numéro de 
+        		téléphone afin d’obtenir des informations ou d’ajouter votre commentaire. Vous avez également la possibilité 
+        		d’ajouter un numéro afin de compléter la liste actuelle et d’indiquer à d’autres consommateurs les appels ou 
+        		SMS indésirables. Pour en savoir plus sur le SPAM téléphonique ou le SPAM SMS, veuillez consulter les informations 
+        		ci-dessous.</p></div>	
+        		
+        		
+			</div>
+        	</article>
+		
+        </section>
+
+        <section id="section2" class="container-fluid">
+        <div class="row">
+
+        	<article id="section2Article1">
+        	
+        		<h4>Qu’est-ce-qu’un SPAM ?</h4>
+
+        		<p>Le spam vocal est une technique frauduleuse qui consiste à appeler un numéro de téléphone en ne laissant 
+        		sonner qu'une seule fois. Le destinataire, n'ayant pas eu le temps de décrocher, essaie alors de rappeler le 
+        		numéro du "correspondant" qui s'est affiché… et tombe sur un numéro surtaxé.La technique existe depuis plusieurs
+        		années mais les escrocs utilisent de nouvelles ruses pour vous inciter à rappeler les numéros surtaxés.
+				Pour ne pas susciter la méfiance de leurs victimes, les escrocs passent désormais leurs appels depuis des numéros 
+				en 01, 02, 04 etc... plutôt que des 0 899. En confiance, vous rappelez et tombez sur un opérateur qui vous annonce 
+				que vous avez gagné un bon d'achat ou un cadeau. Pour pouvoir toucher votre lot, vous devez appeler un prétendu 
+				service clients (dont le numéro en 0 899 est lui surtaxé). L'objectif des escrocs est simple ; vous faire 
+				passer un maximum de temps au téléphone, pour gagner un maximum d’argent à chaque appel. 
+				C'est pour cela qu'ils ont remplacé les ordinateurs, qui passaient des milliers de coups de fil chaque jour, 
+				par des humains.</p>
+
+        	</article>
+
+
+
+        	<article id="section2Article2">
+
+        		<h4>Exemples de SPAMS :</h4>
+
+        		<h5>SMS</h5>
+
+        		<p>“Coin-RELAIS. Bonjour, Votre colis ref:48FQ07 est a votre disposition depuis le 05-01-17. 
+        		Pour confirmer votre adresse, composez le 0899XXXXXX (134e +0.3”</p>
+
+        		<h5>APPEL</h5>
+        	
+				<p>“Bonjour, c’est Alain, je vous appelle à propos de votre assurance habitation. 
+					Rappelez-moi rapidement et si possible même dans la demi-heure au 08 92 XX XX XX”</p>
+
+        	</article>
+
+
+        </div>
+        </section>
 
 
 
